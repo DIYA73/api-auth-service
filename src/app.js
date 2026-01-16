773d13a08183882f2be13869105ddcc9922e7303
DIYA73 <diyaismahil7@gmail.com>
2026-01-17 02:38:04 +0300

    Day 3: JWT login and protected routes working

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const authRoutes = require("./config/routes/auth.routes");
+const protectedRoutes = require("./config/routes/protected.routes");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api", protectedRoutes);
 
 module.exports = app;
--- src/config/controllers/auth.controller.js
+++ src/config/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
+// =====================
+// REGISTER
+// =====================
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -34,6 +38,57 @@ exports.register = async (req, res) => {
         name: user.name,
         email: user.email
       }
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+};
+
+// =====================
+// LOGIN
+// =====================
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Validate
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required"
+      });
+    }
+
+    // 2. Check user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    // 3. Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    // 4. Generate token
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    // 5. Response
+    res.status(200).json({
+      message: "Login successful",
+      token
     });
 
   } catch (error) {
